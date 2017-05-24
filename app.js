cb51d4f8f6e6f08f1b9ab1aa2338abd63eafc8dd
Timon Lukas <timon.lukas3@gmail.com>
2017-05-24 10:50:56 +0200

    Add new test data

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 #!/usr/bin/env nodejs
-const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-http.createServer((request, response) => {
-    response.writeHead(200, {
-        "Content-Type": "text/plain"
-    });
-    response.end("Hello World!");
-}).listen(8080, "localhost");
+const port = process.env.PORT || 8080;
+
+const app = express();
+app.use(bodyParser.urlencoded({
+    extended: false,
+}));
+app.use(bodyParser.json());
+
+app.use((request, response) => {
+    console.log(JSON.stringify(request.body, null, 2));
+    response.end();
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
 
 console.log("Server running at http://localhost:8080/");
