c83df588c08e545d4fbe6b910ee46586525dd7b4
Timon Lukas <timon.lukas3@gmail.com>
2017-05-24 13:05:40 +0200

    Apply standard style

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
 #!/usr/bin/env nodejs
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 8080;
 
 const app = express();
-app.use(bodyParser.urlencoded({
-    extended: false,
-}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use((request, response) => {
-    console.log(JSON.stringify(request.body, null, 2));
-    response.end();
+  console.log(JSON.stringify(request.body, null, 2));
+  response.end();
 });
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
-
-console.log("Server running at http://localhost:8080/");
