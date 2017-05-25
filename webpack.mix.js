6ba190f033a9d078eef8a7a4c606ca60c7f314dd
Timon Lukas <timon.lukas3@gmail.com>
2017-05-25 14:38:41 +0200

    Add footer

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,5 @@ let mix = require('laravel-mix');
 
 mix.js('src/app.js', 'public/')
   .copy('src/index.html', 'public/')
+  .copy('src/style.css', 'public/')
   .copyDirectory('src/favicon', 'public/favicon');
